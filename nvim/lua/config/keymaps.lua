d7871e63d49a7ceae42b551a5a0fcc475a0273ea
marcusluis-s <marcusluis.mail@gmail.com>
2024-07-24 20:35:56 -0300

    keymaps for move between open tabs and move lines up and down

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -12,3 +12,20 @@ keymap('n', '<C-l>', '<C-w>l', opts)
 keymap('n', '<leader>sh', ':split<CR>', opts)
 -- Split window vertically
 keymap('n', '<leader>sv', ':vsplit<CR>', opts)
+
+-- Move lines up and down using Ctrl+k and Ctrl+j
+-- Normal mode mappings
+keymap('n', '<C-j>', ':m .+1<CR>==', opts)
+keymap('n', '<C-k>', ':m .-2<CR>==', opts)
+
+-- Insert mode mappings
+keymap('i', '<C-j>', '<Esc>:m .+1<CR>==gi', opts)
+keymap('i', '<C-k>', '<Esc>:m .-2<CR>==gi', opts)
+
+-- Visual mode mappings
+keymap('v', '<C-j>', ":m '>+1<CR>gv=gv", opts)
+keymap('v', '<C-k>', ":m '<-2<CR>gv=gv", opts)
+
+-- Navigate between open tabs
+keymap('n', '<leader>h', 'gT', opts)
+keymap('n', '<leader>l', 'gt', opts)
