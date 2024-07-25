eb6790d74a65b495f17d418a33f407f93d3dd4f1
marcusluis-s <marcusluis.mail@gmail.com>
2024-07-24 22:46:15 -0300

    keymaps to show diagnostics in a custom float window

--- nvim/lua/plugins/nvim-lsp.lua
+++ nvim/lua/plugins/nvim-lsp.lua
@@ -1,3 +1,15 @@
+-- Function to show diagnostics in a floating window with custom options
+function _G.open_diagnostics_float()
+    vim.diagnostic.open_float(nil, {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        prefix = "",
+        scope = "line",
+    })
+end
+
 return {
     "neovim/nvim-lspconfig",
 
@@ -5,11 +17,35 @@ return {
         local lspconfig = require("lspconfig")
         local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+        -- Key mappings for diagnostics
+        local opts = { noremap=true, silent=true }
+
         lspconfig.clangd.setup({
             capabilities = lsp_capabilities
         })
         lspconfig.tsserver.setup({
             capabilities = lsp_capabilities
         })
+
+ 
+
+        -- Show diagnostics in a floating window
+        vim.api.nvim_set_keymap("n", "<leader>e", "<cmd>lua open_diagnostics_float()<CR>", opts)
+
+        -- Navigate to the previous diagnostic
+        vim.api.nvim_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+
+        -- Navigate to the next diagnostic
+        vim.api.nvim_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+
+        -- Key mappings for LSP functionality
+        vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+        vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+        vim.api.nvim_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+        vim.api.nvim_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+        vim.api.nvim_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+        vim.api.nvim_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+        vim.api.nvim_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+        vim.api.nvim_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
     end
 }
