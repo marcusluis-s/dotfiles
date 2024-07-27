e25afe180d804d552865b083e6d372fdcb2291c0
marcusluis-s <marcusluis.mail@gmail.com>
2024-07-26 21:52:16 -0300

    create a dir called utils with a code to save the last cursor position

--- nvim/lua/utils/save-the-last-cursor-pos.lua
+++ nvim/lua/utils/save-the-last-cursor-pos.lua
@@ -1,49 +1,19 @@
--- Function to save the last position
-local function save_last_position()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    if line > 1 or col > 0 then
-        vim.b.last_cursor_pos = {line, col}
-    end
-end
-
--- Function to restore the last position
-local function restore_last_position()
-    local last_pos = vim.b.last_cursor_pos
-    if last_pos then
-        local line, col = unpack(last_pos)
-        vim.api.nvim_win_set_cursor(0, {line, col})
-    end
-end
-
--- Create autocommands to save and restore position
-vim.api.nvim_create_autocmd("BufReadPost", {
-    pattern = "*",
-    callback = function()
-        local line = vim.fn.line("'\"")
-        if line > 1 and line <= vim.fn.line("$") then
-            vim.api.nvim_win_set_cursor(0, {line, 0})
-            vim.cmd("normal! zvzz")
-        end
+vim.api.nvim_create_autocmd('BufRead', {
+    callback = function(opts)
+        vim.api.nvim_create_autocmd('BufWinEnter', {
+            once = true,
+            buffer = opts.buf,
+            callback = function()
+                local ft = vim.bo[opts.buf].filetype
+                local last_known_line = vim.api.nvim_buf_get_mark(opts.buf, '"')[1]
+                if
+                    not (ft:match('commit') and ft:match('rebase'))
+                    and last_known_line > 1
+                    and last_known_line <= vim.api.nvim_buf_line_count(opts.buf)
+                then
+                    vim.api.nvim_feedkeys([[g`"]], 'nx', false)
+                end
+            end,
+        })
     end,
 })
-
-vim.api.nvim_create_autocmd("BufWinLeave", {
-    pattern = "*",
-    callback = function()
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        if line > 1 or col > 0 then
-            vim.cmd("normal! m\"")
-        end
-    end,
-})
-
-vim.api.nvim_create_autocmd("BufWritePost", {
-    pattern = "*",
-    callback = save_last_position,
-})
-
-vim.api.nvim_create_autocmd("BufReadPost", {
-    pattern = "*",
-    callback = restore_last_position,
-})
-
