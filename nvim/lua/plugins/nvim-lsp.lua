f3430a392a4c0a88a60cd9ef0d3ba41719205665
marcusluis-s <marcusluis.mail@gmail.com>
2024-08-07 10:06:11 -0300

    choosing a differente key combination for display lsp signature help

--- nvim/lua/plugins/nvim-lsp.lua
+++ nvim/lua/plugins/nvim-lsp.lua
@@ -48,7 +48,7 @@ return {
         vim.api.nvim_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
         vim.api.nvim_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
         vim.api.nvim_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-        vim.api.nvim_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+        vim.api.nvim_set_keymap("n", "<leader>k", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
         vim.api.nvim_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
         vim.api.nvim_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
     end
