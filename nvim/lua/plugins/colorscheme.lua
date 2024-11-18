22e4c0c2f17730de4656a2a1cf65f7e260ab7c83
marcusluis-s <marcusluis.mail@gmail.com>
2024-11-18 01:13:06 -0300

    theme tokyonight-night

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,8 @@ return {
     lazy = false,
     priority = 1000,
     opts = {},
+
     config = function()
-        vim.cmd[[colorscheme tokyonight-night]]
+        vim.cmd [[colorscheme tokyonight-night]]
     end
 }
