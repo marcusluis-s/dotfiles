cd255aaa8427c2cfcc62ba86ab9a5ab4ffa9fba0
marcusluis-s <marcusluis.mail@gmail.com>
2025-07-11 21:52:34 -0300

    NeoVim - Here We Go Again

---
+++ nvim/lua/ms/core/keymaps.lua
@@ -0,0 +1,55 @@
+local opts = { noremap = true, silent = true }
+local map = vim.keymap.set
+
+vim.g.mapleader = " "				-- Set leader key to space
+vim.g.maplocalleader = " "			-- Set local leader key to space
+
+-- Fast saving
+map('n', '<C-s>', ':w<CR>', { desc = 'Save file (Ctrl-S)' })
+
+-- Window navigation (using Ctrl + h/j/k/l)
+map('n', '<C-h>', '<C-w>h', { desc = 'Move to left window' })
+map('n', '<C-j>', '<C-w>j', { desc = 'Move to down window' })
+map('n', '<C-k>', '<C-w>k', { desc = 'Move to up window' })
+map('n', '<C-l>', '<C-w>l', { desc = 'Move to right window' })
+
+-- Resize windows
+map('n', '<Leader>wh', '<C-w><', { desc = 'Decrease window width' })
+map('n', '<Leader>wl', '<C-w>>', { desc = 'Increase window width' })
+map('n', '<Leader>wk', '<C-w>+', { desc = 'Increase window height' })
+map('n', '<Leader>wj', '<C-w>-', { desc = 'Decrease window height' })
+map('n', '<Leader>w=', '<C-w>=', { desc = 'Make windows equal size' })
+
+-- New window splits
+map('n', '<Leader>wv', ':vsplit<CR>', { desc = 'Split window vertically' })
+map('n', '<Leader>ws', ':split<CR>', { desc = 'Split window horizontally' })
+
+-- Buffer navigation
+map('n', '[b', ':bprevious<CR>', { desc = 'Go to previous buffer' })
+map('n', ']b', ':bnext<CR>', { desc = 'Go to next buffer' })
+map('n', '<Leader>bc', ':bdelete<CR>', { desc = 'Close current buffer' })
+
+-- Searching
+map('n', '<Leader>h', ':noh<CR>', { desc = 'Clear search highlight' })
+
+-- Move selected lines up/down (Visual mode)
+map('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move selected lines down' })
+map('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move selected lines up' })
+
+-- Move current line up/down (Normal mode)
+map('n', 'J', ':m .+1<CR>==', { desc = 'Move current line down' })
+map('n', 'K', ':m .-2<CR>==', { desc = 'Move current line up' })
+
+-- Delete single character without copying to register
+map('n', 'x', '"_x', { desc = 'Delete character without yank' })
+
+-- Global word replace (prompts for replacement, using common 's' for substitute)
+map('n', '<Leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Global word replace (current word)' })
+
+-- Tab Maps
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>")   --open new tab
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>") --close current tab
+vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>")     --go to next
+vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>")     --go to pre
+vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>") --open current tab in new tab
+
---
+++ nvim/lua/ms/core/options.lua
@@ -0,0 +1,63 @@
+vim.cmd("let g:netrw_banner = 0")
+
+-- vim.g.loaded_netrw = 1				-- Disable Netrw
+-- vim.g.loaded_netrwPlugin = 1		-- Disable Netrw
+
+-- vim.opt.guicursor = ""				-- Change cursor to block intead of line
+vim.opt.guicursor = "n-v-c-sm:block,i:ver25,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250"
+vim.opt.number = true				-- Show line numbers
+vim.opt.relativenumber = true			-- Show relative line numbers
+
+vim.opt.backup = false				-- Do not create backup files
+vim.opt.writebackup = false			-- Do not create backup files when overwriting
+vim.opt.swapfile = false			-- Do not create swap files
+
+-- vim.opt.undofile = true			-- Enable persistent undo
+-- vim.opt.undodir = vim.fn.stdpath("data") .. "/undodir" -- Set undo directory
+
+vim.opt.hlsearch = true				-- Highlight all matches for the current seatch pattern
+vim.opt.incsearch = true			-- Show partial matches while typing seatch pattern
+vim.opt.inccommand = "split"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.clipboard = "unnamedplus"		-- Allows NeoVim to use the system clipboard
+
+vim.opt.scrolloff = 8				-- Lines of context arount the cursor when scrolling
+vim.opt.sidescrolloff = 8			-- Columns of context arount the cursor when horizontally scrolling
+
+vim.opt.expandtab = true			-- Use spaces instead of tabs
+vim.opt.tabstop = 4					-- Number of spaces a tab count for
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4				-- Number of spaces to use for each step (auto)indent
+
+vim.opt.autoindent = true			-- Copy indent from current line when starting a new line
+vim.opt.smartindent = true			-- Smart auto-indenting for C-like languages
+
+vim.opt.wrap = false				-- Do not wrap long lines
+
+vim.opt.mouse = "a"					-- Enable mouse support in all modes
+
+vim.opt.termguicolors = true		-- Enable true colors support
+vim.opt.background = "dark"
+
+vim.opt.showmode = false			-- Don't show the current mode (e.g., -- INSERT --) as it's often redundant with statusline plugin
+
+vim.opt.signcolumn = "yes"			-- Always show the sign column, even if empty
+
+vim.opt.updatetime = 300			-- Faster completion (ms to wait for the file to be written to disk)
+
+vim.opt.timeoutlen = 500			-- Time in ms to wait for a mapped sequence to complete
+
+vim.opt.completeopt = "menu,menuone,noselect"		-- Autocomplete options
+vim.opt.pumheight = 10				-- Maximum number of items to show in the popup menu
+
+vim.opt.backspace = {"start", "eol", "indent"}
+
+vim.opt.confirm = true				-- Ask for confirmation when closing unsaved buffers
+
+vim.opt.splitbelow = true			-- New windows split below the current one
+vim.opt.splitright = true			-- New windows split to the right of the current one
+
+vim.opt.diffopt:append("vertical")	-- Show diffs vertical splits by default
+
