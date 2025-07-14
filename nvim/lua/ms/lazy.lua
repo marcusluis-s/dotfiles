241ee236fc4de7971aee335b7fcb810bdb4c0371
marcusluis-s <marcusluis.mail@gmail.com>
2025-07-14 15:23:06 -0300

    add mason-lspconfig plugin

--- nvim/lua/ms/lazy.lua
+++ nvim/lua/ms/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
   spec = {
     -- import your plugins
     { import = "ms.plugins" },
+    { import = "ms.plugins.lsp" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
